32e0ceb9a1344b9bac13846869ef77266531a739
Yoav Caspi <yocaspi@cisco.com>
2019-12-01 08:48:20 +0200

    solution for day1 part 2

--- day1/solution.py
+++ day1/solution.py
@@ -5,7 +5,24 @@ from typing import List
 def sol1(data: List[str]) -> int:
     res = 0
     for line in data:
-        res += int(line) // 3 -2
+        res += int(line) // 3 - 2
+    return res
+
+
+def calculate_fuel(module_mass: int) -> int:
+    new_mass = module_mass
+    res = 0
+    while new_mass > 0:
+        new_mass = max(new_mass // 3 - 2, 0)
+        res += new_mass
+    return res
+
+
+def sol2(data: List[str]) -> int:
+    res = 0
+    stop_flag = False
+    for line in data:
+        res += calculate_fuel(int(line))
     return res
 
 
@@ -21,6 +38,7 @@ def main() -> int:
     args = parser.parse_args()
     data = get_input(args.filename)
     print(sol1(data))
+    print(sol2(data))
     return 0
 
 
--- day1/test_solution.py
+++ day1/test_solution.py
@@ -1,5 +1,5 @@
 import pytest
-from day1.solution import sol1
+from day1.solution import sol1, sol2
 
 @pytest.mark.parametrize('data, expected',
                          ((['12'], 2),
@@ -9,6 +9,16 @@ from day1.solution import sol1
                           ))
 def test_1(data, expected):
     assert sol1(data) == expected
+
+
+@pytest.mark.parametrize('data, expected',
+                         ((['12'], 2),
+                          (['14'], 2),
+                          (['1969'], 966),
+                          (['100756'], 50346),
+                          ))
+def test_1(data, expected):
+    assert sol2(data) == expected
 
 
 if __name__ == '__main__':
