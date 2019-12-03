89618ee199e2399bf7318ce170cf9ac811a143c0
Yoav Caspi <yocaspi@cisco.com>
2019-12-03 09:07:44 +0200

    solution for day 3 part 2. TODO: need to improve code readability.

--- day3/solution.py
+++ day3/solution.py
@@ -1,18 +1,20 @@
 import argparse
 import sys
 from collections import defaultdict
-from typing import List
+from typing import List, Tuple, Dict
 
 
 class CentralPort:
     def __init__(self):
-        self.current_pos = (0, 0)
-        self.board = defaultdict(lambda: '.')
+        self.current_pos: Tuple[int, int] = (0, 0)
+        self.board: Dict[Tuple[int, int], str] = defaultdict(lambda: '.')
         self.board[(0, 0)] = 'o'
-        self.crossing_points = set()
-        self.closest_point_dist = sys.maxsize
+        self.board_step = {(0, 0): 0}
+        self.wire_step = {'0': 0, '1': 0}
+        self.closest_point_dist: int = sys.maxsize
+        self.closest_steps_dist: int = sys.maxsize
 
-    def move(self, direction: str):
+    def move(self, direction: str, wire_type: str):
         x, y = self.current_pos
         if direction == 'R':
             x += 1
@@ -22,20 +24,23 @@ class CentralPort:
             x -= 1
         elif direction == 'D':
             y -= 1
+        self.wire_step[wire_type] += 1
         self.current_pos = (x, y)
 
     def add_wire(self, direction: str, count: int, wire_type: str):
         if direction not in 'RULD':
             raise ValueError(f"{direction=} not one of RULD")
         for i in range(count):
-            self.move(direction)
+            self.move(direction, wire_type)
             if self.board[self.current_pos] == '.':
                 self.board[self.current_pos] = wire_type
+                self.board_step[self.current_pos] = self.wire_step[wire_type]
             elif self.board[self.current_pos] != wire_type:
                 self.add_crossing_point(self.current_pos)
 
     def add_crossing_point(self, pos):
         self.closest_point_dist = min(self.closest_point_dist, abs(pos[0]) + abs(pos[1]))
+        self.closest_steps_dist = min(self.closest_steps_dist, self.board_step[self.current_pos] + self.wire_step['1'])
 
     def print_board(self):
         min_x = min(x for x, y in self.board.keys())
@@ -58,8 +63,12 @@ def sol1(data: List[str]) -> int:
 
 
 def sol2(data: List[str]) -> int:
-    res = 0
-    return res
+    central_port = CentralPort()
+    for i, directions in enumerate(data):
+        for direction_string in directions.split(','):
+            central_port.add_wire(direction_string[0], int(direction_string[1:]), str(i))
+        central_port.current_pos = (0, 0)
+    return central_port.closest_steps_dist
 
 
 def get_input(filename: str) -> List[str]:
--- day3/test_solution.py
+++ day3/test_solution.py
@@ -1,6 +1,7 @@
 import pytest
 from day3.solution import sol1, sol2
 
+
 @pytest.mark.parametrize('data, expected',
                          ((['R8,U5,L5,D3', 'U7,R6,D4,L4'], 6),
                           (['R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83'], 159),
@@ -8,6 +9,15 @@ from day3.solution import sol1, sol2
                           ))
 def test_1(data, expected):
     assert sol1(data) == expected
+
+
+@pytest.mark.parametrize('data, expected',
+                         ((['R8,U5,L5,D3', 'U7,R6,D4,L4'], 30),
+                          (['R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83'], 610),
+                          (['R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'], 410),
+                          ))
+def test_2(data, expected):
+    assert sol2(data) == expected
 
 
 if __name__ == '__main__':
