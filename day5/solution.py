2ec2f0c2ff32ee8bbf10e51da3d17df2e816040c
Yoav Caspi <yocaspi@cisco.com>
2019-12-05 17:20:05 +0200

    solution for day 5 part 2 (wip)

--- day5/solution.py
+++ day5/solution.py
@@ -10,7 +10,11 @@ class Opcode:
         raise NotImplemented()
 
 
-class Add(Opcode):
+class Operand(Opcode):
+    pass
+
+
+class Add(Operand):
     def __init__(self):
         super().__init__()
         self.instruction_size = 4
@@ -18,7 +22,7 @@ class Add(Opcode):
     def run(self, input_data: List[int], data: List[int], modes: int, *vals) -> Optional[int]:
         assert len(vals) == 3
         retval = 0
-        for i in range(2):
+        for i in range(self.instruction_size - 2):
             modes, mode = divmod(modes, 10)
             if mode == 0:
                 retval += data[vals[i]]
@@ -28,7 +32,7 @@ class Add(Opcode):
         return None
 
 
-class Mul(Opcode):
+class Mul(Operand):
     def __init__(self):
         super().__init__()
         self.instruction_size = 4
@@ -36,7 +40,7 @@ class Mul(Opcode):
     def run(self, input_data: List[int], data: List[int], modes: int, *vals) -> Optional[int]:
         assert len(vals) == 3
         retval = 1
-        for i in range(2):
+        for i in range(self.instruction_size - 2):
             modes, mode = divmod(modes, 10)
             if mode == 0:
                 retval *= data[vals[i]]
@@ -46,7 +50,11 @@ class Mul(Opcode):
         return None
 
 
-class Input(Opcode):
+class Io(Opcode):
+    pass
+
+
+class Input(Io):
     def __init__(self):
         super().__init__()
         self.instruction_size = 2
@@ -57,7 +65,7 @@ class Input(Opcode):
         return None
 
 
-class Output(Opcode):
+class Output(Io):
     def __init__(self):
         super().__init__()
         self.instruction_size = 2
@@ -67,7 +75,88 @@ class Output(Opcode):
         return data[vals[0]]
 
 
-opcodes: Dict[int, Opcode] = dict(((1, Add()), (2, Mul()), (3, Input()), (4, Output()), (99, None)))
+class ConditionalJump(Opcode):
+    def __init__(self):
+        super().__init__()
+        self.instruction_size = 3
+        self.jump_to = -1
+
+    def run(self, input_data: List[int], data: List[int], modes: int, *vals) -> Optional[int]:
+        val: int = 0
+        modes, mode = divmod(modes, 10)
+        if mode == 0:
+            val = data[vals[0]]
+        else:
+            val = vals[0]
+        modes, mode = divmod(modes, 10)
+        self.jump_to = vals[1] if mode else data[vals[1]]
+        return self.condition(val)
+
+    def condition(self, val: int) -> Optional[int]:
+        raise NotImplemented()
+
+    def get_new_ip(self):
+        return self.jump_to
+
+
+class JumpIfTrue(ConditionalJump):
+    def condition(self, val: int) -> bool:
+        return bool(val)
+
+
+class JumpIfFalse(ConditionalJump):
+    def condition(self, val: int) -> bool:
+        return not val
+
+
+class ConditionalStore(Opcode):
+    def __init__(self):
+        super().__init__()
+        self.instruction_size = 4
+
+    def run(self, input_data: List[int], data: List[int], modes: int, *vals) -> Optional[int]:
+        modes, mode = divmod(modes, 10)
+        if mode == 0:
+            val1 = data[vals[0]]
+        else:
+            val1 = vals[0]
+
+        modes, mode = divmod(modes, 10)
+        if mode == 0:
+            val2 = data[vals[1]]
+        else:
+            val2 = vals[1]
+        address = vals[2]
+
+        if self.condition(val1, val2):
+            data[address] = 1
+        else:
+            data[address] = 0
+
+    def condition(self, val1: int, val2: int) -> bool:
+        raise NotImplemented()
+
+
+class LessThan(ConditionalStore):
+    def condition(self, val1: int, val2: int) -> bool:
+        return val1 < val2
+
+
+class Equals(ConditionalStore):
+    def condition(self, val1: int, val2: int) -> bool:
+        return val1 == val2
+
+
+opcodes: Dict[int, Opcode] = dict(
+    ((1, Add()),
+     (2, Mul()),
+     (3, Input()),
+     (4, Output()),
+     (5, JumpIfTrue()),
+     (6, JumpIfFalse()),
+     (7, LessThan()),
+     (8, Equals()),
+     (99, None)))
 
 
 def sol1(data: List[List[int]], input_data: List[int]) -> int:
@@ -88,16 +177,27 @@ def sol1(data: List[List[int]], input_data: List[int]) -> int:
     return retval
 
 
-def sol2(data: List[str]) -> int:
-    expected_data = 19690720
-    for noun in range(100):
-        for verb in range(100):
-            new_data = [int(x) for x in data[0].split(',')]
-            new_data[1] = noun
-            new_data[2] = verb
-            if expected_data == sol1(new_data):
-                return 100 * noun + verb
-    return -1
+def sol2(data: List[List[int]], input_data: List[int]) -> int:
+    retval2 = None
+    for line in data:
+        i = 0
+        size = len(line)
+        while i < size:
+            command = line[i]
+            modes, opcode = divmod(command, 100)
+            method = opcodes[opcode]
+            if method is not None:
+                retval = method.run(input_data, line, modes, *line[i + 1:i + method.instruction_size])
+            else:
+                break
+            if isinstance(method, ConditionalJump) and retval:
+                i = method.get_new_ip()
+            else:
+                i += method.instruction_size
+            if not isinstance(method, ConditionalJump) and retval is not None:
+                print(retval)
+                retval2 = retval
+    return retval2
 
 
 def get_input(filename: str) -> List[str]:
