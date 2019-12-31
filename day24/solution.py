ad3cf7cddc64700c7f95175bafad8518cfdfcb94
Yoav Caspi <yoavcaspi@gmail.com>
2019-12-31 23:32:16 +0200

    solution for day 24 part 2

--- day24/solution.py
+++ day24/solution.py
@@ -38,8 +38,67 @@ def sol1(data: str) -> int:
     return ret_val
 
 
-def sol2(data: str, size: int = 10_007, pos: int = 2019) -> int:
-    return 2
+def sol2(data: str, num_minutes=200) -> int:
+    board: Dict[Tuple[int, int, int], str] = defaultdict(lambda: ".")
+    for y, line in enumerate(data.splitlines()):
+        for x, c in enumerate(line):
+            board[(x, y, 0)] = c
+    for i in range(num_minutes):
+        neighbors_count: Dict[Tuple[int, int, int], int] = defaultdict(int)
+        for j in range((-num_minutes - 1) // 2, (num_minutes + 2) // 2):
+            # regular neighbors
+            for y in range(5):
+                for x in range(5):
+                    if x == 2 and y == 2:
+                        continue
+                    neighbors_count[(x, y, j)] += sum(
+                        (1 for n_x, n_y in neighbors_pos(x, y) if board[(n_x, n_y, j)] == "#"))
+
+            # inner neighbors
+            if board[(2, 1, j)] == "#":
+                for x in range(5):
+                    neighbors_count[(x, 0, j + 1)] += 1
+            if board[(2, 3, j)] == "#":
+                for x in range(5):
+                    neighbors_count[(x, 4, j + 1)] += 1
+            if board[(1, 2, j)] == "#":
+                for y in range(5):
+                    neighbors_count[(0, y, j + 1)] += 1
+            if board[(3, 2, j)] == "#":
+                for y in range(5):
+                    neighbors_count[(4, y, j + 1)] += 1
+
+            # outer neighbors
+            for x in range(5):
+                if board[(x, 0, j)] == "#":
+                    neighbors_count[(2, 1, j - 1)] += 1
+                if board[(x, 4, j)] == "#":
+                    neighbors_count[(2, 3, j - 1)] += 1
+            # outer neighbors
+            for y in range(5):
+                if board[(0, y, j)] == "#":
+                    neighbors_count[(1, 2, j - 1)] += 1
+                if board[(4, y, j)] == "#":
+                    neighbors_count[(3, 2, j - 1)] += 1
+
+        for j in range((-num_minutes - 1) // 2, (num_minutes + 2) // 2):
+            for y in range(5):
+                for x in range(5):
+                    if x == 2 and y == 2:
+                        board[x, y, j] = "."
+                        continue
+                    if board[x, y, j] == "#" and neighbors_count[x, y, j] != 1:
+                        board[x, y, j] = "."
+                    elif board[x, y, j] == "." and neighbors_count[x, y, j] in (1, 2):
+                        board[x, y, j] = "#"
+
+    count = 0
+    for i in range((-num_minutes - 1) // 2, (num_minutes + 2) // 2):
+        for y in range(5):
+            for x in range(5):
+                if board[(x, y, i)] == "#":
+                    count += 1
+    return count
 
 
 def get_input(filename: str) -> str:
--- day24/test_solution.py
+++ day24/test_solution.py
@@ -19,12 +19,13 @@ def test_1(data, expected):
     assert sol1(data) == expected
 
 
-@pytest.mark.parametrize('data, expected',
+@pytest.mark.parametrize('data, num_minutes, expected',
                          (
+                                 (INPUT_A, 10, 99),
                          )
                          )
-def test_2(data, expected):
-    assert sol2(data) == expected
+def test_2(data, num_minutes, expected):
+    assert sol2(data, num_minutes) == expected
 
 
 if __name__ == '__main__':
